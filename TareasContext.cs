cfd929d0ab76a41e4467534b5f491e31e1fcbed7
HaldeFua <haroldareiza89@gmail.com>
2025-03-27 17:46:30 -0500

    Creacion del contexto de la base de datos, con los DbSet y su constructor

---
+++ TareasContext.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+namespace TareasYCategorias.Models;
+
+public class TareasContext : DbContext
+{
+    public DbSet<Categoria> Categoria {get;set;}
+    public DbSet<Tarea> Tareas {get;set;}
+    public TareasContext(DbContextOptions<TareasContext> options) : base(options) { }
+
+}
