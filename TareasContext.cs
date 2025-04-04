af8a8df69a9d9fec2056b7f87f16ebee5643e7c4
Harold Areiza <haroldareiza89@gmail.com>
2025-04-03 19:09:43 -0500

    Edicion y mejora del contexto

--- TareasContext.cs
+++ TareasContext.cs
@@ -1,10 +1,11 @@
+namespace Proyecto;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-namespace Proyecto.Models;
+using Proyecto.Models;
 
 public class TareasContext : DbContext
 {
-    public DbSet<Categoria> Categoria { get; set; }
+    public DbSet<Categoria> Categorias { get; set; }
     public DbSet<Tarea> Tareas { get; set; }
     public TareasContext(DbContextOptions<TareasContext> options) : base(options) { }
 
